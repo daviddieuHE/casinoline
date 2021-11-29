6ecc15c54b4f19d2864569046315e0eecbde82f4
daviddieu <d.dieu@students.ephec.be>
2021-11-29 08:09:50 +0100

    test

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import gameMaster
 
 if __name__ == "__main__":
-    #gameMaster.askPlayerInfos()
-    #gameMaster.askPlayerBet()
+    gameMaster.askPlayerInfos()
+    gameMaster.askPlayerBet()
 
     gameMaster.turnOfWeel()
     gameMaster.check()
